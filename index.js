6909a0ae286ff0ab4ed9e3128c2ed4720ab59af7
flambeauaristide <83838364+flambeauaristide@users.noreply.github.com>
2022-10-11 13:59:14 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var uwindow = document.querySelector('.users-window');
+function show_hide() {
+    if(uwindow.style.display==="none") {
+        uwindow.style.display="block";
+    } else {
+        uwindow.style.display="none";
+    }
+}
